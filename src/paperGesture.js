515b29ec96aac2f0c5cf34707c9fdbb604d63b29
Ray Nimalan <raymondnimalan@gmail.com>
2024-03-05 14:44:29 -0500

    fix: improve paper gesture description

--- src/paperGesture.js
+++ src/paperGesture.js
@@ -7,7 +7,7 @@ import {
 
 const paperDesciption = new GestureDescription('paper');
 
-//all fingers are not curled
+//all fingers are not curled and fingers pointed up or diagonal
 for (let finger of [
   Finger.Thumb,
   Finger.Index,
@@ -15,10 +15,12 @@ for (let finger of [
   Finger.Ring,
   Finger.Pinky,
 ]) {
-  paperDesciption.addCurl(finger, FingerCurl.NoCurl, 1.0);
-  paperDesciption.addDirection(finger, FingerDirection.VerticalUp, 1.0);
-  paperDesciption.addDirection(finger, FingerDirection.DiagonalUpLeft, 1.0);
-  paperDesciption.addDirection(finger, FingerDirection.DiagonalUpRight, 1.0);
-  paperDesciption.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
-  paperDesciption.addDirection(finger, FingerDirection.HorizontalRight, 1.0);
+  paperDesciption.addCurl(finger, FingerCurl.NoCurl, 0.9);
+  paperDesciption.addDirection(finger, FingerDirection.VerticalUp, 0.9);
+  paperDesciption.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.8);
+  paperDesciption.addDirection(finger, FingerDirection.DiagonalUpRight, 0.8);
+  //   paperDesciption.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
+  //   paperDesciption.addDirection(finger, FingerDirection.HorizontalRight, 1.0);
 }
+
+export default paperDesciption;
